81c35a72ed4dd36ac875df2b21e8321a83978175
opiumfive <allswell@bk.ru>
2018-09-11 23:13:06 +0300

    added explosions

--- app/src/main/java/com/opiumfive/gameofballs2/scenes/GameScene.java
+++ app/src/main/java/com/opiumfive/gameofballs2/scenes/GameScene.java
@@ -5,15 +5,26 @@ import com.opiumfive.gameofballs2.managers.SceneManager;
 import com.opiumfive.gameofballs2.units.Ball;
 
 import org.andengine.engine.handler.IUpdateHandler;
+import org.andengine.engine.handler.timer.ITimerCallback;
+import org.andengine.engine.handler.timer.TimerHandler;
+import org.andengine.entity.particle.ParticleSystem;
+import org.andengine.entity.particle.SpriteParticleSystem;
+import org.andengine.entity.particle.emitter.PointParticleEmitter;
+import org.andengine.entity.particle.initializer.VelocityParticleInitializer;
+import org.andengine.entity.particle.modifier.AlphaParticleModifier;
+import org.andengine.entity.particle.modifier.ScaleParticleModifier;
 import org.andengine.entity.scene.IOnAreaTouchListener;
 import org.andengine.entity.scene.IOnSceneTouchListener;
 import org.andengine.entity.scene.ITouchArea;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.background.Background;
+import org.andengine.entity.sprite.Sprite;
 import org.andengine.entity.util.FPSLogger;
 import org.andengine.input.touch.TouchEvent;
 import org.andengine.util.color.Color;
 import org.andengine.util.math.MathUtils;
+
+import java.util.Random;
 
 
 public class GameScene extends BaseScene implements IOnSceneTouchListener, IOnAreaTouchListener {
@@ -40,38 +51,39 @@ public class GameScene extends BaseScene implements IOnSceneTouchListener, IOnAr
         registerUpdateHandler(new IUpdateHandler() {
 
             @Override
-            public void reset() {}
+            public void reset() {
+            }
 
             @Override
             public void onUpdate(float pSecondsElapsed) {
 
-                secElapsed+=pSecondsElapsed;
+                secElapsed += pSecondsElapsed;
+
                 if (secElapsed >= 0.25f) {
                     secElapsed = 0.0f;
                     counter++;
-                    if (counter == thresh+1) counter =0;
+                    if (counter == thresh + 1) counter = 0;
 
                     gameTime++;
-                    if ((MathUtils.RANDOM.nextFloat() < d)||(gameTime-lastball>4)) {
+                    if ((MathUtils.RANDOM.nextFloat() < d) || (gameTime - lastball > 4)) {
                         addBall();
-                        lastball=gameTime;
+                        lastball = gameTime;
                     }
-                    if (d<1) {
-                        d=d+0.001f;
+
+                    if (d < 1) {
+                        d = d + 0.001f;
                     }
                 }
-
-
             }
         });
 
     }
 
     public void addBall() {
-        float width = mResourceManager.mBall.getWidth()/2.0f;
-        float height = mResourceManager.mBall.getHeight()/2.0f;
-        float postX = width +(SCREEN_WIDTH - width*2.0f)* MathUtils.RANDOM.nextFloat();
-        float postY = height+(SCREEN_HEIGHT - height*2.0f)*MathUtils.RANDOM.nextFloat();
+        float width = mResourceManager.mBall.getWidth() / 2.0f;
+        float height = mResourceManager.mBall.getHeight() / 2.0f;
+        float postX = width + (SCREEN_WIDTH - width * 2.0f) * MathUtils.RANDOM.nextFloat();
+        float postY = height + (SCREEN_HEIGHT - height * 2.0f) * MathUtils.RANDOM.nextFloat();
         Ball ball = new Ball(postX, postY, mResourceManager.mBall, mVertexBufferObjectManager);
         registerTouchArea(ball);
         attachChild(ball);
@@ -108,8 +120,32 @@ public class GameScene extends BaseScene implements IOnSceneTouchListener, IOnAr
     @Override
     public boolean onAreaTouched(TouchEvent pSceneTouchEvent, ITouchArea pTouchArea, float pTouchAreaLocalX, float pTouchAreaLocalY) {
         if (pSceneTouchEvent.isActionDown()) {
+            Ball ball = (Ball) pTouchArea;
+            makeExplosion(ball.getX() + ball.getWidth() / 2, ball.getY() + ball.getHeight() / 2, 0.2f + new Random().nextFloat() / 0.8f);
             remove((Ball) pTouchArea);
         }
         return false;
+    }
+
+    private void makeExplosion(float posX, float posY, float strength) {
+        PointParticleEmitter particleEmitter = new PointParticleEmitter(posX, posY);
+
+        final ParticleSystem particleSystem = new SpriteParticleSystem(particleEmitter, 100, 100, 25,
+                mResourceManager.mParticle, mVertexBufferObjectManager);
+
+        particleSystem.addParticleInitializer(new VelocityParticleInitializer<Sprite>( - 150 * strength, 150 * strength, - 150 * strength, 150 * strength));
+        particleSystem.addParticleModifier(new AlphaParticleModifier<Sprite>(0f, 1f, 1f, 0f));
+        particleSystem.addParticleModifier(new ScaleParticleModifier<Sprite>(0f, 1f, 1.5f, 0.8f));
+
+        attachChild(particleSystem);
+        registerUpdateHandler(new TimerHandler(1.0f, new ITimerCallback() {
+            @Override
+            public void onTimePassed(final TimerHandler pTimerHandler) {
+                particleSystem.setIgnoreUpdate(true);
+                particleSystem.detachSelf();
+                GameScene.this.sortChildren();
+                GameScene.this.unregisterUpdateHandler(pTimerHandler);
+            }
+        }));
     }
 }
